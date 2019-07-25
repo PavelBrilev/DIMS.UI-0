02a16aa04093491b45ab274004a6045cdde33712
maximsan <maximuzz@yandex.ru>
2019-07-25 22:26:42 +0300

    rewrite app to functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      Hello!<span>World</span>
-      <br />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    Hello!
+    <span>World</span>
+    <br />
+  </div>
+);
 
 export default App;
