dfeccf8cec79159eadff1a1d77ac36fed0d37e98
Pasha <pasha.brilev@gmail.com>
2019-09-16 17:12:39 +0300

    delete comments

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -37,36 +37,4 @@ class Popup extends React.Component {
 }
 
 
-// class Popup extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { showPopup: false };
-
-//     this.togglePopup = this.togglePopup.bind(this);
-//   }
-
-//   togglePopup() {
-//     this.setState({
-//       showPopup: !this.state.showPopup,
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <Button name={this.props.name} onClick={this.togglePopup} />
-
-//         {this.state.showPopup ? (
-//           <div className='popup'>
-//             <div className='popup_inner'>
-//               {this.props.form}
-//               <Button onClick={this.togglePopup} name='Back to grid' />
-//             </div>
-//           </div>
-//         ) : null}
-//       </>
-//     );
-//   }
-// }
-
 export default Popup;
