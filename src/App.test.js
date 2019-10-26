bd9e54a8c889f99e274bc4523db0312c29911548
maximsan <maximuzz@yandex.ru>
2019-10-27 01:31:36 +0300

    add react-redux-mock dependency

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import { render } from '@testing-library/react';
 import App from './App';
 
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
 describe('<App/>', () => {
   it('Renders without crashing', () => {
-    const { getByText } = render(<App />);
+    const initialState = {
+      /*expected state goes here */
+    };
+    const store = mockStore(initialState);
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
     expect(getByText('DIMS React App')).toBeInTheDocument();
   });
 });
