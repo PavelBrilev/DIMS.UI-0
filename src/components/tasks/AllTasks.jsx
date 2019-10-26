ffaac7423b0540c8a7a1f420f502b2a566a43340
maximsan <maximuzz@yandex.ru>
2019-10-26 22:20:27 +0300

    push travis

--- src/components/tasks/AllTasks.jsx
+++ src/components/tasks/AllTasks.jsx
@@ -1,106 +1,17 @@
 import React, { useRef, useState, useCallback } from 'react';
-import HTML5Backend from 'react-dnd-html5-backend';
-import { useDrag, useDrop, DndProvider } from 'react-dnd';
+// import HTML5Backend from 'react-dnd-html5-backend';
+// import { useDrag, useDrop, DndProvider } from 'react-dnd';
 import { Table } from 'reactstrap';
 import PropTypes from 'prop-types';
 import update from 'immutability-helper';
 import Popup from '../popup/Popup';
 import TasksForm from '../common/forms/tasks-form/TasksForm';
-import DeleteForm from '../common/forms/delete-form/DeleteForm';
 import { Consumer } from '../../App';
 import { icons } from '../common/icons';
 import storage from '../../storage';
+import Task from './Task';
 
 import '../../styles/styles.css';
-
-const ItemTypes = {
-  TASK: 'task',
-};
-
-const style = {
-  padding: '0.5rem 1rem',
-  marginBottom: '.5rem',
-  cursor: 'grab',
-};
-
-const Task = ({ id, task, index, moveTask, setTasks, tasks }) => {
-  const ref = useRef(null);
-  const [, drop] = useDrop({
-    accept: ItemTypes.TASK,
-    hover(item, monitor) {
-      if (!ref.current) {
-        return;
-      }
-      const dragIndex = item.index;
-      const hoverIndex = index;
-      if (dragIndex === hoverIndex) {
-        return;
-      }
-      const hoverBoundingRect = ref.current.getBoundingClientRect();
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      const clientOffset = monitor.getClientOffset();
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-      moveTask(dragIndex, hoverIndex);
-      item.index = hoverIndex;
-    },
-  });
-  const [{ isDragging }, drag] = useDrag({
-    item: { type: ItemTypes.TASK, id, index },
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
-  const opacity = isDragging ? 0.3 : 1;
-  drag(drop(ref));
-  return (
-    <tr key={task.id} ref={ref} style={{ ...style, opacity }}>
-      <td>{task.id}</td>
-      <td>{task.taskName}</td>
-      <td>{task.start}</td>
-      <td>{task.deadline}</td>
-      <td>
-        <Popup key={`${task.id}-1`} icon={icons.editIcon} name='Edit'>
-          <TasksForm
-            setNewTasks={(task) =>
-              setTasks(
-                tasks.map((item) => {
-                  if (item.id === task.id) {
-                    item = task;
-                  }
-                  return item;
-                }),
-              )
-            }
-            id={task.id}
-          />
-        </Popup>
-        <Popup
-          className='btn btn-outline-danger'
-          icon={icons.deleteIcon}
-          name='Delete'
-          id={task.id}
-          key={`${task.id}-2`}
-        >
-          <DeleteForm
-            type='task'
-            setNewState={(task) =>
-              setTasks(tasks.filter((item) => item.id !== task))
-            }
-            id={task.id}
-            name={task.taskName}
-          />
-        </Popup>
-      </td>
-    </tr>
-  );
-};
 
 const AllTasks = () => {
   const [tasks, setTasks] = useState(storage.getTasks());
@@ -130,46 +41,46 @@ const AllTasks = () => {
   };
   if (!tasks || tasks.length === 0) {
     return (
-      <DndProvider backend={HTML5Backend}>
-        <div className='container'>
-          <Popup
-            className='btn btn-outline-primary btn-block'
-            icon={icons.create}
-            name='Create'
-          >
-            <TasksForm setNewTasks={(task) => setTasks(tasks.concat(task))} />
-          </Popup>
-          <p className='text'>No tasks</p>
-        </div>
-      </DndProvider>
+      // <DndProvider backend={HTML5Backend}>
+      <div className='container'>
+        <Popup
+          className='btn btn-outline-primary btn-block'
+          icon={icons.create}
+          name='Create'
+        >
+          <TasksForm setNewTasks={(task) => setTasks(tasks.concat(task))} />
+        </Popup>
+        <p className='text'>No tasks</p>
+      </div>
+      // </DndProvider>
     );
   }
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className='container'>
-        <Popup name='Create'>
-          <TasksForm setNewTasks={(task) => setTasks(tasks.concat(task))} />
-        </Popup>
-        <Consumer>
-          {(theme) => (
-            <DndProvider backend={HTML5Backend}>
-              <Table hover id={`${theme}`}>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Start</th>
-                    <th>Deadline</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>{tasks.map((task, i) => renderTask(task, i))}</tbody>
-              </Table>
-            </DndProvider>
-          )}
-        </Consumer>
-      </div>
-    </DndProvider>
+    // <DndProvider backend={HTML5Backend}>
+    <div className='container'>
+      <Popup name='Create'>
+        <TasksForm setNewTasks={(task) => setTasks(tasks.concat(task))} />
+      </Popup>
+      <Consumer>
+        {(theme) => (
+          // <DndProvider backend={HTML5Backend}>
+          <Table hover id={`${theme}`}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Start</th>
+                <th>Deadline</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>{tasks.map((task, i) => renderTask(task, i))}</tbody>
+          </Table>
+          //</DndProvider>
+        )}
+      </Consumer>
+    </div>
+    // </DndProvider>
   );
 };
 
